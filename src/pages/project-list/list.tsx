aa35e0605fa6c92153935bea3aceaa09cfcad1df
2405693142@qq.com <2405693142@qq.com>
2022-05-17 10:45:00 +0800

    feat:svg

--- src/pages/project-list/list.tsx
+++ src/pages/project-list/list.tsx
@@ -3,7 +3,7 @@ import { Table} from 'antd'
 export const  List =({projectList,users}:{ projectList:projectType[] , users:userType[]}) =>{
   return (<Table pagination={ false } columns={[{title:"名称",dataIndex:"name",sorter:(a,b)=>a.name.localeCompare(b.name)},{
     title:"负责人",
-    render(value , project){
+    render(_ , project){
       return (
         <span>
           { users.find(user=>user.id === project.personId)?.name || "未知"}
--- src/unAuth-app/index.tsx
+++ src/unAuth-app/index.tsx
@@ -3,16 +3,41 @@ import { LoginPage } from '../pages/login';
 import { RegisterPage } from './register'
 
 import { Card , Button} from 'antd'
+import styled from '@emotion/styled';
+
+import logo  from '../assets/logo.svg';
+import right  from '../assets/right.svg';
+import left  from '../assets/left.svg';
 export const UnauthApp =()=>{
   const [ isRegister , setRegister] = useState( false)
   return (
-    <div>
-      <Card>
+    <Container>
+      <ShadowCard>
         <div style={{ display:"flex" ,flexDirection:"column", justifyContent:"center" ,alignItems:"center"}}>
         { isRegister? <RegisterPage></RegisterPage> : <LoginPage></LoginPage> }
         <Button style={{ margin:" .625rem 0"}} onClick={()=>setRegister( !isRegister ) }> 切换 { isRegister ? "登陆" : "注册" }</Button>
         </div>
-      </Card>
-    </div>
+      </ShadowCard>
+    </Container>
   )
 }
+
+const Header = styled.header`
+  background: url(${logo});
+`
+
+const ShadowCard = styled(Card)`
+  width: 40rem;
+  min-height:56rem ;
+  padding:3.2rem 4rem ;
+  border-radius: 0.3rem;
+  box-shadow:rgba(0,0,0,0.1) 0 0 10px ;
+  text-align:center ;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction:column ;
+  align-items:center ;
+  min-height: 100vh;
+`
