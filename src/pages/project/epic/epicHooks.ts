150713d2790a2b8b884a4f392b27c0ceddca037c
2405693142@qq.com <2405693142@qq.com>
2022-05-24 22:11:36 +0800

    feat:epic delete Optimistic Update

---
+++ src/pages/project/epic/epicHooks.ts
@@ -0,0 +1,55 @@
+
+import { QueryKey, useMutation, useQuery } from "react-query";
+import { useHttp } from "../../../hooks/https";
+import { Task } from "../../../types/task";
+import { useProject } from "../../project-list/projectHooks";
+import { useAddUpdate, useDeleteUpdate, useEditUpdate, useProjectIdInUrl } from "../signboard/signboardHooks";
+
+export interface Epic{
+  id:number
+  name:string
+  projectId:number
+  start:number
+  end:number
+}
+
+
+export const useEpics = (param?:Partial<Task>)=>{
+  const client = useHttp()
+  return useQuery<Epic[]>(["epics",param],()=>client("epics",{data:param}))
+}
+
+export const useEpicById = ()=>useProject(useProjectIdInUrl())
+
+export const useEpicSearchParams =()=>({projectId:useProjectIdInUrl()})
+export const useEpicQueryKey = ()=>["epics",useEpicSearchParams()]
+
+export const useAddEpic = (queryKey:QueryKey)=>{
+const client = useHttp()
+return useMutation(
+  (params:Partial<Epic>)=>client("epics",{
+    method:"POST",
+    data:params
+  }),
+  useAddUpdate(queryKey)  
+)
+}
+export const useDeleteEpic = (queryKey:QueryKey)=>{
+const client = useHttp()
+return useMutation(
+  ({id}:{id:number})=>client(`epics/${id}`,{
+    method:"DELETE",
+  }),
+  useDeleteUpdate(queryKey)
+)
+}
+export const useEditEpic = (queryKey:QueryKey)=>{
+const client = useHttp()
+return useMutation(
+  (params:Partial<Task>)=>client(`tasks/${params.id}`,{
+    method:"PATCH",
+    data:params
+  }),
+  useEditUpdate(queryKey)
+)
+}
---
+++ src/pages/project/signboard/taskHooks.ts
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+import { useQuery } from "react-query";
+import { useDebounce, useQueryParam } from "../../../hooks";
+import { useHttp } from "../../../hooks/https";
+import { Task, TaskType } from "../../../types/task";
+import { useProject } from "../../project-list/projectHooks";
+import { useProjectIdInUrl } from "./signboardHooks";
+
+
+export const useTasks = (param?:Partial<Task>)=>{
+  const client = useHttp()
+  return useQuery<Task[]>(["tasks",param],()=>client("tasks",{data:param}))
+}
+
+export const useTaskById = ()=>useProject(useProjectIdInUrl())
+
+export const useTaskSearchParams =()=>{
+  const [ param ] = useQueryParam(["name","typeId","processorId","tagId"])
+  const projectId = useProjectIdInUrl()
+  const taskName = useDebounce(param.name , 300)
+  return useMemo(()=>({
+    projectId,
+    name:taskName,
+    typeId:Number(param.typeId) || undefined,
+    processorId:Number(param.processorId) || undefined,
+    tagId:Number(param.tagId) || undefined
+  }),[projectId,param,taskName])
+}
+
+export const useTaskQueryKey = ()=>["tasks",useTaskSearchParams()]
+
+export const useTaskTypes = ()=>{
+  const client = useHttp()
+  return useQuery<TaskType[]>(["taskTypes"],()=>client("taskTypes"))
+}
