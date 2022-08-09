056647a7c1ac52fe0a42ca7f7830fff95d4fcbd8
CCherry07 <2405693142@qq.com>
2022-08-09 19:54:52 +0800

    chore:  change code style

--- src/pages/project-list/list.tsx
+++ src/pages/project-list/list.tsx
@@ -1,77 +1,80 @@
-import {projectType , userType} from './type'
-import { Table , Dropdown , Button ,Menu , Modal} from 'antd'
+import { projectType, userType } from './type'
+import { Table, Dropdown, Button, Menu, Modal } from 'antd'
 import dayjs from 'dayjs'
 
-import {TableProps } from 'antd/es/table'
+import { TableProps } from 'antd/es/table'
 import { Link } from 'react-router-dom'
 import { Pin } from '../../components/components'
 import { useDeleteProjectWithQuery, useEditProjectWithQuery, useProjectsModal, useProjectsQueryKey } from './projectHooks'
-interface ListProps extends TableProps<projectType>  {
-  users:userType[]
-  reFresh?:()=>void
+interface ListProps extends TableProps<projectType> {
+  users: userType[]
+  reFresh?: () => void
 }
-export const  List =({ users, ...props }:ListProps) =>{
+export const List = ({ users, ...props }: ListProps) => {
 
   // const { muTate } = useEditProject()
   const { mutate } = useEditProjectWithQuery(useProjectsQueryKey())
-  const pinProject =(id:number)=>(pin:boolean)=>mutate({id,pin})
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin })
 
-  
-  return (<Table pagination={ false } columns={
+
+  return (<Table pagination={false} columns={
     [
       {
-        title:<Pin checked={true} disabled={true}/>,
-        render(value,project){
-          return <Pin checked={project.pin} onCheckedChange={pin=>pinProject(project.id)(pin)}></Pin>
+        title: <Pin checked={true} disabled={true} />,
+        render(value, project) {
+          return <Pin checked={project.pin} onCheckedChange={pin => pinProject(project.id)(pin)}></Pin>
         }
       },
-      {title:"名称",dataIndex:"name",
-    sorter:(a,b)=>a.name.localeCompare(b.name),
-    render(value , project){
-      return(
-        <Link key={project.id} to={`${project.id}`}>{project.name}</Link>
-      )
-    }
-    },
-    { title:"部门",dataIndex:"organization" },{
-    title:"负责人",
-    render(_ , project){
-      return (
-        <span key={project.id}>
-          { users.find(user=>user.id === project.personId)?.name || "未知"}
-        </span>
-      )
-    }
-  },{ title:"创建时间" , render(_,project){
-    return (
-      <span key={project.id}>
-        { project.created ? dayjs(project.created).format("YYYY-DD-mm"):null}
-      </span>
-    )
-  }},{
-    title:"Control",
-    render(value,project){
-      return (
-        <ControlDropdown project={project}></ControlDropdown>
-      )
-    }
-  }]} {...props}>
+      {
+        title: "名称", dataIndex: "name",
+        sorter: (a, b) => a.name.localeCompare(b.name),
+        render(value, project) {
+          return (
+            <Link key={project.id} to={`${project.id}`}>{project.name}</Link>
+          )
+        }
+      },
+      { title: "部门", dataIndex: "organization" }, {
+        title: "负责人",
+        render(_, project) {
+          return (
+            <span key={project.id}>
+              {users.find(user => user.id === project.personId)?.name || "未知"}
+            </span>
+          )
+        }
+      }, {
+        title: "创建时间", render(_, project) {
+          return (
+            <span key={project.id}>
+              {project.created ? dayjs(project.created).format("YYYY-DD-mm") : null}
+            </span>
+          )
+        }
+      }, {
+        title: "Control",
+        render(value, project) {
+          return (
+            <ControlDropdown project={project}></ControlDropdown>
+          )
+        }
+      }]} {...props}>
   </Table>)
 }
 
 
-const ControlDropdown = ({project}:{project:projectType})=>{
-  const {startEdit} = useProjectsModal()
-  const editProject =(id:number)=>()=>startEdit(id)
-  const { mutate:deleteProject} = useDeleteProjectWithQuery(useProjectsQueryKey())
-  const confirmDeleteProject = (id:number,projectName:string)=>{
+const ControlDropdown = ({ project }: { project: projectType }) => {
+  const { startEdit } = useProjectsModal()
+  const editProject = (id: number) => () => startEdit(id)
+  const { mutate: deleteProject } = useDeleteProjectWithQuery(useProjectsQueryKey())
+  const confirmDeleteProject = (id: number, projectName: string) => {
     Modal.confirm({
-      title:`确定要删除这个<${projectName}>项目吗？`,
+      title: `确定要删除这个<${projectName}>项目吗？`,
       content: `点击确定删除 <${projectName}>`,
-      okText:"确定",
-      cancelText:"取消",
-      onOk(){
-        deleteProject({id})
+      okText: "确定",
+      cancelText: "取消",
+      onOk() {
+        deleteProject({ id })
       }
     })
   }
@@ -81,7 +84,7 @@ const ControlDropdown = ({project}:{project:projectType})=>{
         <Button type='link' onClick={editProject(project.id)}>编辑</Button>
       </Menu.Item>
       <Menu.Item key={"delete"}>
-      <Button type='link' onClick={()=>(confirmDeleteProject(project.id,project.name))}>删除</Button>
+        <Button type='link' onClick={() => (confirmDeleteProject(project.id, project.name))}>删除</Button>
       </Menu.Item>
     </Menu>
   }>
