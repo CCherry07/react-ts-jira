eb93774b19eecbfdace4592b06fbc659b63c89b2
CCherry07 <2405693142@qq.com>
2022-08-11 21:54:12 +0800

    style:  code style

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthProvider } from "./auth-context";
-export const AppProviders =( {children}:{children:ReactNode})=>{
+export const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
-   <QueryClientProvider client={new QueryClient()}>
-    <AuthProvider>
-     { children }
-    </AuthProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
--- src/pages/project/searchPanel.tsx
+++ src/pages/project/searchPanel.tsx
@@ -1,30 +1,30 @@
-import { Input , Button } from 'antd'
+import { Input, Button } from 'antd'
 import { Row, UserSelect } from '../../components/components'
 import { useSetUrlSearchParam } from '../../hooks'
 import { useTaskSearchParams } from './signboard/taskHooks'
 import { TaskSelect } from './signboard/components'
-export const SearchPanel = ()=>{
-  const searchParams  = useTaskSearchParams()
+export const SearchPanel = () => {
+  const searchParams = useTaskSearchParams()
   const setSearchParams = useSetUrlSearchParam()
-  const rest=()=>{
+  const rest = () => {
     setSearchParams({
-      name:"",
-      typeId:"",
-      processorId:"",
-      tagId:""
+      name: "",
+      typeId: "",
+      processorId: "",
+      tagId: ""
     })
   }
 
   return (<Row marginBottom={3} justifyContent="normal" marginRight={2}>
-    <Input  style={{width:"20rem"}} placeholder="任务名" 
-    value={searchParams.name} onChange={evt=>setSearchParams({name:evt.target.value})}></Input>
+    <Input style={{ width: "20rem" }} placeholder="任务名"
+      value={searchParams.name} onChange={evt => setSearchParams({ name: evt.target.value })}></Input>
 
-    <UserSelect defaultOptionName='经办人' 
-    value={searchParams.processorId} onChange={value=>setSearchParams({processorId:value})}>
+    <UserSelect defaultOptionName='经办人'
+      value={searchParams.processorId} onChange={value => setSearchParams({ processorId: value })}>
     </UserSelect>
-    
+
     <TaskSelect defaultOptionName='类型'
-     value={searchParams.typeId} onChange={value=>setSearchParams({typeId:value})}>
+      value={searchParams.typeId} onChange={value => setSearchParams({ typeId: value })}>
     </TaskSelect>
     <Button type='default' onClick={rest} > 清除筛选器</Button>
   </Row>)
