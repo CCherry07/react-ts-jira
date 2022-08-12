9369a8f8f609d9f660fd41f165c84391860abf49
CCherry07 <2405693142@qq.com>
2022-08-12 22:33:49 +0800

    style:  code style

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,41 +1,40 @@
-import {Popover , Typography ,List   ,Button} from 'antd'
-import {useProjects, useUsers } from '../hooks';
+import { Popover, Typography, List, Button } from 'antd'
+import { useProjects, useUsers } from '../hooks';
 
-import {useProjectsModal, useProjectsSearchParams } from './project-list/projectHooks';
-export const ProjectPopover = ()=>{
-  const {open} = useProjectsModal()
-  // const {data:projects ,retry} = useData<projectType[]>({remainingUrl:"projects"})
-    const {data:projects , refetch} = useProjects(useProjectsSearchParams()[0])
+import { useProjectsModal, useProjectsSearchParams } from './project-list/projectHooks';
+export const ProjectPopover = () => {
+  const { open } = useProjectsModal()
+  const { data: projects, refetch } = useProjects(useProjectsSearchParams()[0])
   const pinnedProjects = projects?.filter(project => project.pin)
-  const content = (<div style={{width:"20rem"}}>
+  const content = (<div style={{ width: "20rem" }}>
     <Typography.Text type='secondary'>收藏项目</Typography.Text>
     <List>
-      {  
-        pinnedProjects?.map(project=>(
+      {
+        pinnedProjects?.map(project => (
           <List.Item key={project.id}>
             <List.Item.Meta title={project.name}></List.Item.Meta>
           </List.Item>
         ))
       }
-      <Button style={{padding:0}} type='link' onClick={()=>open()}> 创建项目 </Button>
+      <Button style={{ padding: 0 }} type='link' onClick={() => open()}> 创建项目 </Button>
     </List>
   </div>)
   return (
-    <Popover onVisibleChange={()=>refetch()} placement='bottom' content={content}>
-      <h3 style={{cursor:"pointer"}}> ITEMS </h3>
+    <Popover onVisibleChange={() => refetch()} placement='bottom' content={content}>
+      <h3 style={{ cursor: "pointer" }}> ITEMS </h3>
     </Popover>
   )
 }
 
 
-export const UserPopover = ()=>{
+export const UserPopover = () => {
   // const {data:projects ,retry} = useData<projectType[]>({remainingUrl:"projects"})
-    const {data:users , refetch} = useUsers()
-    const content = (<div style={{width:"20rem"}}>
+  const { data: users, refetch } = useUsers()
+  const content = (<div style={{ width: "20rem" }}>
     <Typography.Text type='secondary'>组员列表</Typography.Text>
     <List>
       {
-        users?.map(user=>(
+        users?.map(user => (
           <List.Item key={user.id}>
             <List.Item.Meta title={user.name}></List.Item.Meta>
           </List.Item>
@@ -44,8 +43,8 @@ export const UserPopover = ()=>{
     </List>
   </div>)
   return (
-    <Popover onVisibleChange={()=>refetch()} placement='bottom' content={content}>
-      <h3 style={{cursor:"pointer"}}> USERS </h3>
+    <Popover onVisibleChange={() => refetch()} placement='bottom' content={content}>
+      <h3 style={{ cursor: "pointer" }}> USERS </h3>
     </Popover>
   )
 }
--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,40 +1,40 @@
-import { Menu} from 'antd'
+import { Menu } from 'antd'
 import styled from '@emotion/styled'
-import { Link ,Routes,Route ,Navigate, useLocation} from 'react-router-dom'
-import {PageEpic} from './epic'
-import {PageSignboard} from './signboard'
+import { Link, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { PageEpic } from './epic'
+import { PageSignboard } from './signboard'
 
-const useCurrentRouteName = () =>{
+const useCurrentRouteName = () => {
   const units = useLocation().pathname.split("/")
   return units[units.length - 1]
 }
 
-export const PageProject =()=>{
+export const PageProject = () => {
   const currentRouteName = useCurrentRouteName()
   return (
     <Container>
       <Aside>
         <Menu mode='inline' selectedKeys={[currentRouteName]}>
           <Menu.Item key={"signboard"}>
-            <Link to={"signboard"}>SIGNBOARD</Link> 
+            <Link to={"signboard"}>SIGNBOARD</Link>
           </Menu.Item>
           <Menu.Item key={"epic"}>
-          <Link to={"epic"}>MISSIONBOARD</Link>
+            <Link to={"epic"}>MISSIONBOARD</Link>
           </Menu.Item>
         </Menu>
       </Aside>
-       <Main>
-       <Routes>
+      <Main>
+        <Routes>
           <Route path='signboard' element={<PageSignboard></PageSignboard>}></Route>
           <Route path='epic' element={<PageEpic></PageEpic>}></Route>
-          <Route path="*" element={<Navigate to={ window.location.pathname + "/signboard" } replace={true}/>} />
+          <Route path="*" element={<Navigate to={window.location.pathname + "/signboard"} replace={true} />} />
         </Routes>
-       </Main>
+      </Main>
     </Container>
   )
 }
 
-const Container =styled.div`
+const Container = styled.div`
   display:grid ;
   grid-template-columns:16rem 1fr ;
   overflow: hidden;
