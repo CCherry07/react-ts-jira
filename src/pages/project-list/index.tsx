6a993782251e6bc4da820c5c7c94672a268b012c
CCherry07 <2405693142@qq.com>
2022-08-14 22:49:58 +0800

    style:  code style

--- src/pages/project-list/index.tsx
+++ src/pages/project-list/index.tsx
@@ -1,21 +1,21 @@
 import styled from '@emotion/styled'
-import  { Button } from 'antd'
-import {userType } from './type'
+import { Button } from 'antd'
+import { userType } from './type'
 
 import { SearchPanel } from './search-panel'
-import { List} from './list'
+import { List } from './list'
 
 import { useData, useDebounce, useProjects } from '../../hooks'
 import { useProjectsModal, useProjectsSearchParams } from './projectHooks'
 import { Row, ShowError } from '../../components/components'
-export const ProjectListScreen = ()=>{
+export const ProjectListScreen = () => {
   const { open } = useProjectsModal()
   //劫持param 使其停止输入 delay 后更新
-  const[param , setParam] = useProjectsSearchParams()
+  const [param, setParam] = useProjectsSearchParams()
   // const { data:projectList , isLoading ,retry} = useData<projectType[]>(
   // { remainingUrl:"projects" ,queryOptions:useDebounce(param , 800)})
-  const { data:projectList ,isLoading ,error ,isError} = useProjects(useDebounce(param , 800))  
-  const{data:users} = useData<userType[]>({ remainingUrl:"users"})
+  const { data: projectList, isLoading, error, isError } = useProjects(useDebounce(param, 800))
+  const { data: users } = useData<userType[]>({ remainingUrl: "users" })
   return (
     <Container>
       <Row>
@@ -24,9 +24,9 @@ export const ProjectListScreen = ()=>{
           创建项目
         </Button>
       </Row>
-      <SearchPanel users={ users|| [] } param={param} setParam={setParam} ></SearchPanel>
-      {isError? <ShowError error={error}/>
-      :<List loading={isLoading} dataSource={projectList||[]} users={users || []}/>}
+      <SearchPanel users={users || []} param={param} setParam={setParam} ></SearchPanel>
+      {isError ? <ShowError error={error} />
+        : <List loading={isLoading} dataSource={projectList || []} users={users || []} />}
     </Container>
   )
 }
