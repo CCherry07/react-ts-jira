d0fe4513fd56ca5e25a6b3c4cd50a38eb523ca42
CCherry07 <2405693142@qq.com>
2022-08-15 22:32:06 +0800

    style:  code style

--- src/pages/project-list/projectHooks.ts
+++ src/pages/project-list/projectHooks.ts
@@ -1,91 +1,94 @@
 import { projectType } from "./type"
-import { useAsync, useQueryParam, useSetUrlSearchParam} from "../../hooks"
+import { useAsync, useQueryParam, useSetUrlSearchParam } from "../../hooks"
 import { useHttp } from "../../hooks/https";
 import { useMemo } from "react";
 import { QueryKey, useMutation, useQuery, useQueryClient } from "react-query";
 
-export const useEditProject = () =>{
-    const { run,...asyncResult } = useAsync()
-    const client = useHttp();
-    const muTate = (params:Partial<projectType>)=>{
-        console.log(params);
-        return run(client(`projects/${params.id}`,{
-          method:"PATCH",
-          data:params}))
-    }
-    return {
-      muTate,
-      ...asyncResult
-    }
+export const useEditProject = () => {
+  const { run, ...asyncResult } = useAsync()
+  const client = useHttp();
+  const muTate = (params: Partial<projectType>) => {
+    console.log(params);
+    return run(client(`projects/${params.id}`, {
+      method: "PATCH",
+      data: params
+    }))
+  }
+  return {
+    muTate,
+    ...asyncResult
+  }
 }
 
-export const useEditProjectWithQuery = (queryKey:QueryKey)=>{
+export const useEditProjectWithQuery = (queryKey: QueryKey) => {
   const client = useHttp()
   return useMutation(
-    (params:Partial<projectType>)=>client(`projects/${params.id}`,{
-      method:"PATCH",
-      data:params
+    (params: Partial<projectType>) => client(`projects/${params.id}`, {
+      method: "PATCH",
+      data: params
     }),
     useEditUpdate(queryKey)
   )
 }
-export const useDeleteProjectWithQuery = (queryKey:QueryKey)=>{
+export const useDeleteProjectWithQuery = (queryKey: QueryKey) => {
   const client = useHttp()
   return useMutation(
-    ({id}:{id:number})=>client(`projects/${id}`,{
-      method:"DELETE",
+    ({ id }: { id: number }) => client(`projects/${id}`, {
+      method: "DELETE",
     }),
     useDeleteUpdate(queryKey)
   )
 }
-export const useAddProject = () =>{
-    const { run,...asyncResult } = useAsync()
-    const client = useHttp();
-    const muTate = (params:Partial<projectType>)=>{
-        return run(client(`projects/${params.id}`,{
-          method:"POST",
-          data:params
-        }))
-    }
-    return {
-      muTate,
-      ...asyncResult
-    }
+export const useAddProject = () => {
+  const { run, ...asyncResult } = useAsync()
+  const client = useHttp();
+  const muTate = (params: Partial<projectType>) => {
+    return run(client(`projects/${params.id}`, {
+      method: "POST",
+      data: params
+    }))
+  }
+  return {
+    muTate,
+    ...asyncResult
+  }
 }
 
-export const useConfig = (queryKey:QueryKey , callback:(target:any,old?:any[])=>any[])=>{
+export const useConfig = (queryKey: QueryKey, callback: (target: any, old?: any[]) => any[]) => {
   const queryClient = useQueryClient()
-  return{
-    onSuccess:()=>queryClient.invalidateQueries("projects"),
-    async onMutate(target:any){
+  return {
+    onSuccess: () => queryClient.invalidateQueries("projects"),
+    async onMutate(target: any) {
       const previousItems = queryClient.getQueriesData(queryKey)
-      queryClient.setQueryData(queryKey,(old?:any[])=>{
-        return callback(target , old)
+      queryClient.setQueryData(queryKey, (old?: any[]) => {
+        return callback(target, old)
       })
-      return{previousItems}
+      return { previousItems }
     },
-    onError(error:any,newItem:any,context:any){
-      queryClient.setQueryData(queryKey,(context as { previousItems:projectType[]}).previousItems)
+    onError(error: any, newItem: any, context: any) {
+      queryClient.setQueryData(queryKey, (context as { previousItems: projectType[] }).previousItems)
     }
   }
 }
 
-export const useDeleteUpdate = (queryKey:QueryKey)=>
-        useConfig(queryKey,(target,old)=>old?.filter(item=>item.id !== target.id)||[])
-export const useEditUpdate = (queryKey:QueryKey)=>
-        useConfig(queryKey,(target,old)=>old?.map(item=>item.id === target.id?{...item
-        ,...target}:item)||[])
-export const useAddUpdate = (queryKey:QueryKey)=>useConfig(queryKey,(target,old)=>old?[...old,target] : [])
+export const useDeleteUpdate = (queryKey: QueryKey) =>
+  useConfig(queryKey, (target, old) => old?.filter(item => item.id !== target.id) || [])
+export const useEditUpdate = (queryKey: QueryKey) =>
+  useConfig(queryKey, (target, old) => old?.map(item => item.id === target.id ? {
+    ...item
+    , ...target
+  } : item) || [])
+export const useAddUpdate = (queryKey: QueryKey) => useConfig(queryKey, (target, old) => old ? [...old, target] : [])
 
 // 使用usequery
-export const useAddProjectWithQuery = (queryKey:QueryKey)=>{
+export const useAddProjectWithQuery = (queryKey: QueryKey) => {
   const client = useHttp()
   return useMutation(
-    (params:Partial<projectType>)=>client("projects",{
-      method:"POST",
-      data:params
+    (params: Partial<projectType>) => client("projects", {
+      method: "POST",
+      data: params
     }),
-    useAddUpdate(queryKey)  
+    useAddUpdate(queryKey)
   )
 }
 
@@ -100,34 +103,34 @@ export const useProjectsSearchParams = () => {
   ] as const;
 };
 
-export const useProjectsQueryKey=()=>["projects",useProjectsSearchParams()[0]]
+export const useProjectsQueryKey = () => ["projects", useProjectsSearchParams()[0]]
 
-export const useProject = (id?:number)=>{
+export const useProject = (id?: number) => {
   const client = useHttp()
   return useQuery<projectType>(
-    ["project",{id}],
-    ()=>client(`projects/${id}`),
+    ["project", { id }],
+    () => client(`projects/${id}`),
     {
-      enabled:!!id
+      enabled: !!id
     }
   )
 }
 
 
 // 控制 modal 页打开
-export const useProjectsModal = ()=>{
-  const [{projectCreate},setProjectCreate] = useQueryParam(["projectCreate"])
-  const [{editingProjectId},setEditingProjectId] = useQueryParam(["editingProjectId"])
-  const {data:editingProject,isLoading} = useProject(Number(editingProjectId))
+export const useProjectsModal = () => {
+  const [{ projectCreate }, setProjectCreate] = useQueryParam(["projectCreate"])
+  const [{ editingProjectId }, setEditingProjectId] = useQueryParam(["editingProjectId"])
+  const { data: editingProject, isLoading } = useProject(Number(editingProjectId))
   const setUrlSearchParam = useSetUrlSearchParam()
-  const open = ()=>setProjectCreate({projectCreate:true})
-  const close = ()=>setUrlSearchParam({projectCreate:"",editingProjectId:""})
-  
-  const startEdit = (id:number)=>setEditingProjectId({editingProjectId:id})
+  const open = () => setProjectCreate({ projectCreate: true })
+  const close = () => setUrlSearchParam({ projectCreate: "", editingProjectId: "" })
+
+  const startEdit = (id: number) => setEditingProjectId({ editingProjectId: id })
   return {
     projectCreateOpen: projectCreate === "true" || !!editingProjectId,
-    editingProject,isLoading,
-    open,close,startEdit
+    editingProject, isLoading,
+    open, close, startEdit
   }
 }
 
