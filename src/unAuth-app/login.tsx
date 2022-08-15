90a1d893f605a57e820da0a8a68cd2e76b931fa1
CCherry07 <2405693142@qq.com>
2022-08-16 06:30:20 +0800

    style:  code style

--- src/unAuth-app/login.tsx
+++ src/unAuth-app/login.tsx
@@ -1,20 +1,20 @@
 import { useAuth } from '../context/auth-context'
-import { Form  , Input} from 'antd'
-import {LongButton } from '.'
+import { Form, Input } from 'antd'
+import { LongButton } from '.'
 import { useAsync } from '../hooks'
-export const LoginPage =({onError}:{onError:(error:Error)=>void})=>{
-  const {login } = useAuth()
-  const { run,isLoading } = useAsync(undefined , { processErrorBySelf:true })
-  const handleSubmit = async (value:{ username:string , password:string })=>{
+export const LoginPage = ({ onError }: { onError: (error: Error) => void }) => {
+  const { login } = useAuth()
+  const { run, isLoading } = useAsync(undefined, { processErrorBySelf: true })
+  const handleSubmit = async (value: { username: string, password: string }) => {
     run(login(value)).catch(onError)
   }
   return (
-    <Form style={{display:"flex", flexDirection:"column",justifyContent:"center"}} onFinish={ handleSubmit }>
-      <Form.Item name={ "username" } rules={[ {required:true , message:"请输入用户名"}]}>
-        <Input placeholder='请输入用户名...' type="text" id="username"/>
+    <Form style={{ display: "flex", flexDirection: "column", justifyContent: "center" }} onFinish={handleSubmit}>
+      <Form.Item name={"username"} rules={[{ required: true, message: "请输入用户名" }]}>
+        <Input placeholder='请输入用户名...' type="text" id="username" />
       </Form.Item>
-      <Form.Item name={"password"} rules={[{required:true , message:"请输入密码"}]}>
-        <Input placeholder='请输入密码...' type="password" id="password"/>
+      <Form.Item name={"password"} rules={[{ required: true, message: "请输入密码" }]}>
+        <Input placeholder='请输入密码...' type="password" id="password" />
       </Form.Item>
       <LongButton loading={isLoading} htmlType="submit" type="primary">登陆</LongButton>
     </Form>
