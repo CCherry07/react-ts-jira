e38cbd449e65459af53ceabadb16567551023900
CCherry07 <2405693142@qq.com>
2022-08-17 21:17:11 +0800

    style:  code style

--- src/unAuth-app/register.tsx
+++ src/unAuth-app/register.tsx
@@ -1,14 +1,14 @@
 import { useAuth } from '../context/auth-context'
-import { Form  , Input } from 'antd'
-import {useForm} from 'antd/es/form/Form'
+import { Form, Input } from 'antd'
+import { useForm } from 'antd/es/form/Form'
 import { LongButton } from './index'
 
 import { useAsync } from '../hooks'
-export const RegisterPage =({onError}:{onError:(error:Error)=>void})=>{
+export const RegisterPage = ({ onError }: { onError: (error: Error) => void }) => {
   const { register } = useAuth()
-  const { run,isLoading } = useAsync(undefined , { processErrorBySelf:true })
+  const { run, isLoading } = useAsync(undefined, { processErrorBySelf: true })
   const [form] = useForm()
-  const handleSubmit = async (value:{ username:string , password:string ,Cpassword:string})=>{
+  const handleSubmit = async (value: { username: string, password: string, Cpassword: string }) => {
     const password = value.password
     if (password !== value.Cpassword) {
       onError(new Error("请确认两次输入的密码相同"))
@@ -17,18 +17,18 @@ export const RegisterPage =({onError}:{onError:(error:Error)=>void})=>{
     run(register(value)).catch(onError)
   }
   return (
-    <Form form={form} style={{display:"flex", flexDirection:"column",justifyContent:"center"}} onFinish={ handleSubmit }>
-    <Form.Item name={ "username" } rules={[ {required:true , message:"请输入用户名"}]}>
-      <Input placeholder='请输入用户名...' type="text" id="username"/>
-    </Form.Item>
-    <Form.Item name={"password"} rules={[{required:true , message:"请输入密码"}]}>
-      <Input  placeholder='请输入密码...' type="password" id="password"/>
-    </Form.Item>
-    <Form.Item name={"Cpassword"} rules={[{required:true , message:"请确认密码"}]}>
-      <Input  placeholder='请确认密码' type="password" id="Cpassword"/>
-    </Form.Item>
-    <LongButton loading={isLoading} htmlType="submit" type="primary">注册</LongButton>
-  </Form>
+    <Form form={form} style={{ display: "flex", flexDirection: "column", justifyContent: "center" }} onFinish={handleSubmit}>
+      <Form.Item name={"username"} rules={[{ required: true, message: "请输入用户名" }]}>
+        <Input placeholder='请输入用户名...' type="text" id="username" />
+      </Form.Item>
+      <Form.Item name={"password"} rules={[{ required: true, message: "请输入密码" }]}>
+        <Input placeholder='请输入密码...' type="password" id="password" />
+      </Form.Item>
+      <Form.Item name={"Cpassword"} rules={[{ required: true, message: "请确认密码" }]}>
+        <Input placeholder='请确认密码' type="password" id="Cpassword" />
+      </Form.Item>
+      <LongButton loading={isLoading} htmlType="submit" type="primary">注册</LongButton>
+    </Form>
   )
 }
 
