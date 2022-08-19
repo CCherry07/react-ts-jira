f440655ad51715d7ba34910fd673e64b07da5bea
CCherry07 <2405693142@qq.com>
2022-08-19 17:56:11 +0800

    style:  code style

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import './wdyr'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { DevTools ,loadServer} from 'jira-dev-tool'
+import { DevTools, loadServer } from 'jira-dev-tool'
 import { AppProviders } from './context'
 import "antd/dist/antd.less"
-loadServer(()=>{
+loadServer(() => {
   return (ReactDOM.createRoot(document.getElementById('root')!).render(
     <AppProviders>
       <DevTools></DevTools>
-       <App></App> 
+      <App></App>
     </AppProviders>
   ))
 })
-
-
-
-
