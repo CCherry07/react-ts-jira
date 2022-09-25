53a80bfb6f2a40970b49c1f6d09e38d5d65ca2bc
CCherry07 <2405693142@qq.com>
2022-09-25 23:02:20 +0800

    style:  format code

--- src/pages/project/epic/epicHooks.ts
+++ src/pages/project/epic/epicHooks.ts
@@ -4,50 +4,50 @@ import { Task } from "../../../types/task";
 import { useProject } from "../../project-list/projectHooks";
 import { useAddUpdate, useDeleteUpdate, useEditUpdate, useProjectIdInUrl } from "../signboard/signboardHooks";
 
-export interface Epic{
-  id:number
-  name:string
-  projectId:number
-  start:number
-  end:number
+export interface Epic {
+  id: number
+  name: string
+  projectId: number
+  start: number
+  end: number
 }
 
 
-export const useEpics = (param?:Partial<Task>)=>{
+export const useEpics = (param?: Partial<Task>) => {
   const client = useHttp()
-  return useQuery<Epic[]>(["epics",param],()=>client("epics",{data:param}))
+  return useQuery<Epic[]>(["epics", param], () => client("epics", { data: param }))
 }
 
-export const useEpicById = ()=>useProject(useProjectIdInUrl())
+export const useEpicById = () => useProject(useProjectIdInUrl())
 
-export const useEpicSearchParams =()=>({projectId:useProjectIdInUrl()})
-export const useEpicQueryKey = ()=>["epics",useEpicSearchParams()]
+export const useEpicSearchParams = () => ({ projectId: useProjectIdInUrl() })
+export const useEpicQueryKey = () => ["epics", useEpicSearchParams()]
 
-export const useAddEpic = (queryKey:QueryKey)=>{
-const client = useHttp()
-return useMutation(
-  (params:Partial<Epic>)=>client("epics",{
-    method:"POST",
-    data:params
-  }),
-  useAddUpdate(queryKey)  
-)
+export const useAddEpic = (queryKey: QueryKey) => {
+  const client = useHttp()
+  return useMutation(
+    (params: Partial<Epic>) => client("epics", {
+      method: "POST",
+      data: params
+    }),
+    useAddUpdate(queryKey)
+  )
 }
-export const useDeleteEpic = (queryKey:QueryKey)=>{
-const client = useHttp()
-return useMutation(
-  ({id}:{id:number})=>client(`epics/${id}`,{
-    method:"DELETE",
-  }),
-  useDeleteUpdate(queryKey)
-)
+export const useDeleteEpic = (queryKey: QueryKey) => {
+  const client = useHttp()
+  return useMutation(
+    ({ id }: { id: number }) => client(`epics/${id}`, {
+      method: "DELETE",
+    }),
+    useDeleteUpdate(queryKey)
+  )
 }
-export const useEditEpic = (queryKey:QueryKey)=>{
-const client = useHttp()
-return useMutation(
-    (params:Partial<Task>)=>client(`tasks/${params.id}`,{
-      method:"PATCH",
-      data:params
+export const useEditEpic = (queryKey: QueryKey) => {
+  const client = useHttp()
+  return useMutation(
+    (params: Partial<Task>) => client(`tasks/${params.id}`, {
+      method: "PATCH",
+      data: params
     }),
     useEditUpdate(queryKey)
   )
