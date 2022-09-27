71f83ff3232960ff12ee7d32f543ffce1cee02ac
CCherry07 <2405693142@qq.com>
2022-09-27 18:08:19 +0800

    style:  format code

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,24 +1,24 @@
 import { http } from '../../hooks/https'
 import { userType } from '../../pages/project-list/type'
 interface userinfo {
-  username:string,
-  password:string
+  username: string,
+  password: string
 }
 
 const localstoreKey = "__token"
-export const getToken=()=>{
+export const getToken = () => {
   const token = window.localStorage.getItem(localstoreKey)
   return token
 }
 
-const setToken = (token:string)=>{
-  window.localStorage.setItem(localstoreKey , token)
+const setToken = (token: string) => {
+  window.localStorage.setItem(localstoreKey, token)
 }
 
-export const loginWithregister = async (userinfo:userinfo,type:"login" | "register" ):Promise<userType>=>{
+export const loginWithregister = async (userinfo: userinfo, type: "login" | "register"): Promise<userType> => {
   const url = type
-  const {user} = await http(url,{data:userinfo,method:"POST"})
-     setToken(user.token)
-  return http(url,{data:userinfo,method:"POST"})
+  const { user } = await http(url, { data: userinfo, method: "POST" })
+  setToken(user.token)
+  return http(url, { data: userinfo, method: "POST" })
 }
 export const logOut = () => window.localStorage.removeItem(localstoreKey)
