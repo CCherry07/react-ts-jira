9a3eeb4aa7272cd285448d7c387399d12f4b9897
CCherry07 <2405693142@qq.com>
2022-10-09 22:40:42 +0800

    style:  format code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,87 +1,87 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
-import {clearObject, subset} from '../utils'
+import { clearObject, subset } from '../utils'
 import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
 import { useHttp } from "./https"
 import { useQuery } from "react-query"
 import { paramType, projectType, userType } from "../pages/project-list/type"
 
-export const useMount =(cb:()=>void)=>{
-  useEffect(()=>{
+export const useMount = (cb: () => void) => {
+  useEffect(() => {
     cb()
-  },[])
+  }, [])
 }
 
 // 截断 value delay 后更新新的value
-export const useDebounce = function<T>( value:T , delay:number):T{
-  const [ debounceValue , setDebounceValue ] = useState(value)
-  useEffect(()=>{
-    const timer = setTimeout(()=>setDebounceValue(value),delay)
+export const useDebounce = function <T>(value: T, delay: number): T {
+  const [debounceValue, setDebounceValue] = useState(value)
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounceValue(value), delay)
     return () => clearTimeout(timer)
-  },[ value , delay ])
+  }, [value, delay])
   return debounceValue
 }
 
-interface State<D>{
-  error:Error|null
-  data:D | null
-  status:"idle"|"loading"|"error"|"success"
+interface State<D> {
+  error: Error | null
+  data: D | null
+  status: "idle" | "loading" | "error" | "success"
 }
-const defaultInitState:State<null> ={
-  status:"idle",
-  data:null,
-  error:null
+const defaultInitState: State<null> = {
+  status: "idle",
+  data: null,
+  error: null
 }
 
 const defaultConfig = {
-  processErrorBySelf:false
+  processErrorBySelf: false
 }
 
 // processErrorBySelf
-export const useAsync = <D>(initState?:State<D>,initConfig?:typeof defaultConfig)=>{
-  const config = {...defaultConfig , ...initConfig }
-  const [ state , setState ] = useState<State<D>>({
+export const useAsync = <D>(initState?: State<D>, initConfig?: typeof defaultConfig) => {
+  const config = { ...defaultConfig, ...initConfig }
+  const [state, setState] = useState<State<D>>({
     ...defaultInitState,
     ...initState
   })
-  const [ retry , setReTry ] = useState(()=>()=>{})
-  const setData = (data:D) => setState({
+  const [retry, setReTry] = useState(() => () => { })
+  const setData = (data: D) => setState({
     data,
-    status:"success",
-    error:null
+    status: "success",
+    error: null
   })
-  const setError = (error:Error) => setState({
-    data:null,
-    status:"error",
+  const setError = (error: Error) => setState({
+    data: null,
+    status: "error",
     error
-  })  
-  const muntedRef = useMuntedRef() 
+  })
+  const muntedRef = useMuntedRef()
 
-  const run  = useCallback(( promise:Promise<D> ,runConfig?:{ retry:()=>Promise<D> } )=>{
+  const run = useCallback((promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
     if (!promise || !promise.then) {
       throw new Error("please Pass In The PromiseType !");
-    } 
-    setReTry(()=>()=>{
+    }
+    setReTry(() => () => {
       if (runConfig?.retry) {
-        run(runConfig.retry(),runConfig)
+        run(runConfig.retry(), runConfig)
       }
     })
-    setState({...state , status:"loading"})
-    return promise.then(data=>{
+    setState({ ...state, status: "loading" })
+    return promise.then(data => {
       if (muntedRef.current) {
         setData(data)
       }
       return data
-    },(error=>{
+    }, (error => {
       setError(error)
       if (config.processErrorBySelf) return Promise.reject(error)
       return error
     }))
-  },[muntedRef,retry,setData,setError,state])
+  }, [muntedRef, retry, setData, setError, state])
   return {
-    isIdle:state.status ==="idle",
-    isLoading:state.status ==="loading",
-    isError:state.status ==="error",
-    isSuccess:state.status ==="success",
+    isIdle: state.status === "idle",
+    isLoading: state.status === "loading",
+    isError: state.status === "error",
+    isSuccess: state.status === "success",
     run,
     retry,
     setData,
@@ -91,96 +91,96 @@ export const useAsync = <D>(initState?:State<D>,initConfig?:typeof defaultConfig
 }
 
 
-interface useDataParamType{
-  remainingUrl:string,
-  queryOptions?:{ [key: string]: any;}
+interface useDataParamType {
+  remainingUrl: string,
+  queryOptions?: { [key: string]: any; }
 }
 
-export const useData = <T>(parameter:useDataParamType)=>{
-  const {remainingUrl , queryOptions ,...config } = parameter
-  const { run  ,...result} = useAsync<T>()
-  const [restData,setrestData] = useState(false)
+export const useData = <T>(parameter: useDataParamType) => {
+  const { remainingUrl, queryOptions, ...config } = parameter
+  const { run, ...result } = useAsync<T>()
+  const [restData, setrestData] = useState(false)
   const http = useHttp()
-  let retry = () => http(`${parameter.remainingUrl}`,{data:queryOptions,...config})
-  useEffect(()=>{
-    run(retry(),{retry})
-   },[parameter.queryOptions])
-   return {
-     ...result,
-     restData,
-     setrestData
-   }
+  let retry = () => http(`${parameter.remainingUrl}`, { data: queryOptions, ...config })
+  useEffect(() => {
+    run(retry(), { retry })
+  }, [parameter.queryOptions])
+  return {
+    ...result,
+    restData,
+    setrestData
+  }
 }
 
-export const useProjects = (param:Partial<paramType>)=>{
+export const useProjects = (param: Partial<paramType>) => {
   const client = useHttp()
-  return useQuery<projectType[]>(["projects",param],()=>{
-    return client("projects",{data:param})
+  return useQuery<projectType[]>(["projects", param], () => {
+    return client("projects", { data: param })
   })
 }
-export const useUsers = (param?:Partial<userType>)=>{
+export const useUsers = (param?: Partial<userType>) => {
   const client = useHttp()
-  return useQuery<userType[]>(["users",param],()=>{
-    return client("users",{data:param})
+  return useQuery<userType[]>(["users", param], () => {
+    return client("users", { data: param })
   })
 }
 
 
-export const useDocTitle =(title:string,keepOnUnmount:boolean =true)=>{
+export const useDocTitle = (title: string, keepOnUnmount: boolean = true) => {
   //将传进来的title实用useRef 持久化，不受生命周期的影响
   const oldTitle = useRef(document.title).current
   //每一次都更新的doc Title
-  useEffect(()=>{
+  useEffect(() => {
     document.title = title
-  },[title])
+  }, [title])
 
   //页面卸载时，将doc Title 置回前一次的title
-  useEffect(()=>{
-    return ()=>{
+  useEffect(() => {
+    return () => {
       if (!keepOnUnmount) {
         document.title = oldTitle
       }
     }
-  },[keepOnUnmount , oldTitle])
-} 
+  }, [keepOnUnmount, oldTitle])
+}
 
-type ParamType<T extends string> = {[k in T]:string}
+type ParamType<T extends string> = { [k in T]: string }
 
 // 获取query 里面的键值对
-export const useQueryParam =<T extends string>(keys:T[])=>{
+export const useQueryParam = <T extends string>(keys: T[]) => {
   const [searchParams] = useSearchParams()
   const setSearchParams = useSetUrlSearchParam()
   const [stateKeys] = useState(keys)
   return [
     useMemo(
-      ()=>{
-       return subset(Object.fromEntries(searchParams),stateKeys) as ParamType<T>
-        },[ searchParams , stateKeys ]
-      ),
-    (params:Partial<{[key in T]:unknown}> )=>{
-     return setSearchParams(params)
+      () => {
+        return subset(Object.fromEntries(searchParams), stateKeys) as ParamType<T>
+      }, [searchParams, stateKeys]
+    ),
+    (params: Partial<{ [key in T]: unknown }>) => {
+      return setSearchParams(params)
     }
   ] as const
 }
 
 // 更新Url Search query
-export const useSetUrlSearchParam = ()=>{
-  const [searchParams ,setSearchParams] = useSearchParams()
-  return (params:{[key in string]:unknown})=>{
-    const o = clearObject({...Object.fromEntries(searchParams) , ...params}) as URLSearchParamsInit
-      return setSearchParams(o)
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  return (params: { [key in string]: unknown }) => {
+    const o = clearObject({ ...Object.fromEntries(searchParams), ...params }) as URLSearchParamsInit
+    return setSearchParams(o)
   }
 }
 
 
 // usemuntedRef
-export const useMuntedRef = ()=>{
-  const muntedRef = useRef(false) 
-  useEffect(()=>{
-   muntedRef.current = true
-   return ()=> {
-    muntedRef.current = false
-   }
+export const useMuntedRef = () => {
+  const muntedRef = useRef(false)
+  useEffect(() => {
+    muntedRef.current = true
+    return () => {
+      muntedRef.current = false
+    }
   })
   return muntedRef
 }
