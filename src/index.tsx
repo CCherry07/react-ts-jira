bbcc8f02d2ac3af4c320be16f14f7b3fe0330e28
CCherry7 <2405693142@qq.com>
2022-12-07 21:55:07 +0800

    feat:  del

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { DevTools, loadServer } from 'jira-dev-tool'
 import { AppProviders } from './context'
-import "antd/dist/antd.less"
 loadServer(() => {
   return (ReactDOM.createRoot(document.getElementById('root')!).render(
     <AppProviders>
-      <DevTools></DevTools>
       <App></App>
     </AppProviders>
   ))
